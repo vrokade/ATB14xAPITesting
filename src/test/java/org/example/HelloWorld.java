d0ca14f6c3bdf48ee3f34d425c3045cf8f9013de
Vishal.Rokade <vrokade2013@gmail.com>
2025-12-01 17:54:39 +0530

    second commit

--- src/test/java/org/example/HelloWorld.java
+++ src/test/java/org/example/HelloWorld.java
@@ -1,4 +1,13 @@
 package org.example;
 
+import io.restassured.RestAssured;
+
 public class HelloWorld {
+    public static void main(String[] args) {
+
+        RestAssured
+                .given().baseUri("https://google.com").log().all()
+                .when().get()
+                .then().statusCode(200).log().all();
+    }
 }
